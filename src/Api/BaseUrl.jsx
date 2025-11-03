f95f8d5eb7b8fad3f12dd1616700afb17e5ed1e7
Rohit Patidar <ritikpatidar580@gmail.com>
2025-11-03 13:04:47 +0530

    rohit

--- src/Api/BaseUrl.jsx
+++ src/Api/BaseUrl.jsx
@@ -1,4 +1,5 @@
-const BaseUrl = 'https://sbph7x24-8080.inc1.devtunnels.ms/api/v1/';
+// const BaseUrl = 'https://sbph7x24-8080.inc1.devtunnels.ms/api/v1/';
 // const BaseUrl = 'https://main-acconting-production.up.railway.app/api/v1/';
+const BaseUrl = 'https://ssknf82q-8080.inc1.devtunnels.ms/api/v1/';
 
 export default BaseUrl; 
