dec33b8dd7da58dfeeb28be3a453a521bddbb878
Rohit Patidar <ritikpatidar580@gmail.com>
2025-11-25 19:43:53 +0530

    rohit

--- src/Api/BaseUrl.jsx
+++ src/Api/BaseUrl.jsx
@@ -1,6 +1,6 @@
-// const BaseUrl = 'https://accounting-prisma-production.up.railway.app/api/v1/';
+const BaseUrl = 'https://accounting-prisma-production.up.railway.app/api/v1/';
 
-const BaseUrl = 'https://02x4fc84-8080.inc1.devtunnels.ms/api/v1/';
+// const BaseUrl = 'https://02x4fc84-8080.inc1.devtunnels.ms/api/v1/';
 
 // const BaseUrl = 'https://02x4fc84-8080.inc1.devtunnels.ms/api/v1/';
 
